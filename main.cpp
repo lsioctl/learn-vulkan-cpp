f1a2d2f59f9c7f1a4a16a7b936e46d8c398eb3be
lsioctl <laurent.steff@gmail.com>
2025-04-11 23:38:37 -0400

    feat: create instance working

--- main.cpp
+++ main.cpp
@@ -1,35 +1,117 @@
+// Let GLFW include by itslef vulkan headers
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
 
-int main() {
-    glfwInit();
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+void print_extensions() {
+    // retrieve a list of supported extensions
+    // could be compared to glfwGetRequiredInstanceExtensions
 
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+    std::cout << "Available extensions:\n";
 
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    for (const auto& extension : extensions) {
+        std::cout << '\t' << extension.extensionName << '\n';
     }
 
-    glfwDestroyWindow(window);
+}
 
-    glfwTerminate();
 
-    return 0;
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window_;
+    VkInstance instance_;
+    void initWindow() {
+        glfwSetErrorCallback(error_callback);
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window_ = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+        // std::cout << window_ << std::endl;
+    }
+
+    void createInstance() {
+        // The instance is the connection between your application and the Vulkan library
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        // Note: there is a RAII way to do instead of this: find this an migrate
+        // to it when the tutorial is finished
+        if (vkCreateInstance(&createInfo, nullptr, &instance_) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+
+    void initVulkan() {
+        print_extensions();
+        createInstance();
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window_)) {
+        glfwPollEvents();
+    }
+    }
+
+    void cleanup() {
+        // As we do not use RAII for now, destroy is needed
+        vkDestroyInstance(instance_, nullptr);
+        glfwDestroyWindow(window_);
+
+        glfwTerminate();
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
