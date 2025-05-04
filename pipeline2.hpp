cdabfdcffb73d26651e75f1032b52d3f3e3952e0
lsioctl <laurent.steff@gmail.com>
2025-05-04 15:54:24 -0400

    chore: vertex buffer, wip

---
+++ pipeline2.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+// Let GLFW include by itslef vulkan headers
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+namespace pipeline2 {
+
+/** the attachments referenced by the pipeline stages and their usage */
+void createRenderPass(
+    VkDevice logical_device,
+    VkFormat swapChainImageFormat,
+    VkRenderPass* pRenderPass
+);
+
+void createGraphicsPipeline(
+    const char* vert_file,
+    const char* frag_file,
+    VkDevice logical_device,
+    VkExtent2D swapChainExtent,
+    VkRenderPass renderPass,
+    VkPipelineLayout* pPipelineLayout,
+    VkPipeline* pGraphicsPipeline
+);
+
+}
--- shaders/compile1.sh
+++ shaders/compile1.sh
@@ -7,4 +7,5 @@ source ./env
 mkdir -p ${OUTPUT_DIR}
 
 ${GLSLC} -fshader-stage=vert shader1.vert.glsl -o ${OUTPUT_DIR}/shader1.vert.spirv
+${GLSLC} -fshader-stage=vert shader2.vert.glsl -o ${OUTPUT_DIR}/shader2.vert.spirv
 ${GLSLC} -fshader-stage=frag shader1.frag.glsl -o ${OUTPUT_DIR}/shader1.frag.spirv
