3c005ea67a6e683ce10760e58dbb35bed42da487
lsioctl <laurent.steff@gmail.com>
2025-05-10 17:21:38 -0400

    feat: uniforms working

--- pipeline3.cpp
+++ pipeline3.cpp
@@ -235,7 +235,9 @@ void createGraphicsPipeline(
     // thicker than 1 requires wideLines GPU feature
     rasterizer.lineWidth = 1.0f;
     rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
-    rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    // counter clockwise because of the Y-flip in the projection matrix
+    // rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
     rasterizer.depthBiasEnable = VK_FALSE;
     rasterizer.depthBiasConstantFactor = 0.0f; // Optional
     rasterizer.depthBiasClamp = 0.0f; // Optional
