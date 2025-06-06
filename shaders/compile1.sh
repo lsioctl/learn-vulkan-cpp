ee5d009d1cca35ef9930125479462999fd906c69
lsioctl <lsioctl@gmail.com>
2025-06-06 16:45:13 -0400

    chore: texture, wip

--- shaders/compile1.sh
+++ shaders/compile1.sh
@@ -9,4 +9,6 @@ mkdir -p ${OUTPUT_DIR}
 ${GLSLC} -fshader-stage=vert shader1.vert.glsl -o ${OUTPUT_DIR}/shader1.vert.spirv
 ${GLSLC} -fshader-stage=vert shader2.vert.glsl -o ${OUTPUT_DIR}/shader2.vert.spirv
 ${GLSLC} -fshader-stage=vert shader3.vert.glsl -o ${OUTPUT_DIR}/shader3.vert.spirv
+${GLSLC} -fshader-stage=vert shader4.vert.glsl -o ${OUTPUT_DIR}/shader4.vert.spirv
 ${GLSLC} -fshader-stage=frag shader1.frag.glsl -o ${OUTPUT_DIR}/shader1.frag.spirv
+${GLSLC} -fshader-stage=frag shader2.frag.glsl -o ${OUTPUT_DIR}/shader2.frag.spirv
